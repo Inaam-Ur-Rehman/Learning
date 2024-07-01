a39bd1a506cddafcaaa57385dad5e790ebfc4e70
Inaam Ur Rehman <inaamurrehman343@gmail.com>
2024-07-01 09:30:47 +0500

    Strings

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-let person = {
-    name: "Ali",
-    age: 24,
-    getInfo: function (){
-        return "Name of person is: "+ this.name + " and age is: "+ this.age
-    },
-    isLive: true
-}
+let names = "ali, ahmad, umar"
+let firstName = "Ali Ali Ali"
+let lastName = "Ahmad"
+// console.log(names)
 
-// person.email = "test@gmail.com"
+// console.log(names.split(",")[2])
+// console.log(names.charAt(20))
+// console.log(names.charCodeAt(0))
+// console.log(firstName.concat(lastName))
+// console.log(firstName.endsWith("a"))
+// console.log(firstName.startsWith("A"))
+// console.log(firstName.indexOf("A"))
+// console.log(firstName.lastIndexOf("A"))
+// console.log(names.includes("ali"))
 
-// delete person.age
 
-// console.log(person)
-// console.log(person["age"])
-console.log(person.getInfo())
+// regex for password validation
+// min 8 characters, 1 uppercase, 1 lowercase, 1 number, 1 special character
+// let password = "Ali1234"
+// let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+
+// console.log(passwordRegex.match(password))

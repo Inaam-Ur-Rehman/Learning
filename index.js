eba8dfc54b402de6ee60cc91fe04c0f6393575a3
Inaam Ur Rehman <inaamurrehman343@gmail.com>
2024-07-02 08:35:14 +0500

    Initial commit

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-let names = "ali, ahmad, umar"
-let firstName = "Ali Ali Ali"
-let lastName = "Ahmad"
-// console.log(names)
+// print numbers from 1 to 20
 
-// console.log(names.split(",")[2])
-// console.log(names.charAt(20))
-// console.log(names.charCodeAt(0))
-// console.log(firstName.concat(lastName))
-// console.log(firstName.endsWith("a"))
-// console.log(firstName.startsWith("A"))
-// console.log(firstName.indexOf("A"))
-// console.log(firstName.lastIndexOf("A"))
-// console.log(names.includes("ali"))
+// initial value
+// end value (condition)
+// condition
 
+// for(let i = 1; i <= 20; i++) {
+//     console.log(i)
+// }
 
-// regex for password validation
-// min 8 characters, 1 uppercase, 1 lowercase, 1 number, 1 special character
-// let password = "Ali1234"
-// let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+let i = 1;
 
-// console.log(passwordRegex.match(password))
+while(i<=20){
+    console.log(i);
+    i++;
+}

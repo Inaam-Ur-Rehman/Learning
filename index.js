f9d6cf16f9167e554ccb69d472baf3875ba0f57b
Inaam Ur Rehman <inaamurrehman343@gmail.com>
2024-07-08 09:41:07 +0500

    Initaial Commit

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
-function btnClick(){
-    let body = document.querySelector("body");
-    let btn = document.querySelector("button");
-    let isDark = body.classList.contains("dark")
-    if(isDark){
-        body.classList.remove("dark")
-        btn.innerText = "Toggle Dark Theme"
-    }
-    else{
-        body.classList.add("dark")
-        btn.innerText = "Toggle Light Theme"
-    }
-  }
-
 document.addEventListener("DOMContentLoaded", function () {
-  
-  let btn = document.querySelector("button");
-  btn.addEventListener("click", btnClick)
+  let form = document.getElementById("calculator__form");
+
+  let operation;
+
+  let plus = document.getElementById("plus");
+    let minus = document.getElementById("minus");
+    let div = document.getElementById("div");
+    let mul = document.getElementById("mul");
+
+    plus.addEventListener("click",function(){
+      operation = "+"
+    });
+    minus.addEventListener("click",function(){
+      operation = "-"
+    });
+    
+    div.addEventListener("click",function(){
+      operation = "/"
+    });
+    
+    mul.addEventListener("click",function(){
+      operation = "*"
+    });
+
+  form.addEventListener("submit",function(e){
+    e.preventDefault();
+
+     let result = document.getElementById("result");
+    
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    let res = caluculation(num1,num2,operation)
+    result.innerText = "Result: "+ res
+  })  
 });
 
-
-// let btn = document.querySelector("button");
-//   console.log(btn)
-//   btn.addEventListener("dblclick", function () {
-//     alert("Btn Clicked");
-//  });
+function caluculation(num1, num2, op){
+    if(op == "+") {
+        return num1 + num2
+    }
+    else if(op == "-"){
+        return num1 - num2
+    }
+    else if(op == "*"){
+        return num1 * num2
+    }
+    else if(op == "/"){
+        return num1 / num2
+    }
+    else if(op == "%"){
+        return num1 % num2
+    }
+    else{
+        return "Enter valid operation"
+    }
+}
